90cb7626187d3069acd1dafaea172166d2291933
Vishwanth26 <vishwanth5595@gmail.com>
2025-02-20 20:42:08 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,43 @@
+/*Question-1*/
+var anon= function(){
+}
+var arrow = () => {
+}
+
+/*Question-2*/
+ var add=(a, b)=>a + b
+ console.log(add(2,4))
+
+/*Question-3*/
+
+ var square=n=> n * n;
+ console.log(square(4)) 
+
+/*Question-4*/
+
+ var function_name=a=>a*a
+
+/*Question-5*/
+
+ var cube=a=>a**3
+ console.log(cube(2))
+
+ var cube1=a=>{
+    return a**3
+ }
+ console.log(cube1(4))
+
+/*Question-6*/
+
+ var diff=(a,b)=>a-b
+ console.log(diff(2,3))
+
+ var sub1 = (a,b) => {
+    return a-b
+}
+console.log(sub1(4,8))
+
+/*Question-7*/
+
+ var pavani=()=>"hello world!"
+ console.log(pavani())
